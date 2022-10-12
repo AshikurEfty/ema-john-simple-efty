0e6bb649bfde19a58ad2f5d0c6ca602c7eba1b07
AshikurEfty <ashikurefty713@gmai.com>
2022-10-13 02:10:12 +0600

    added cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
-    // const {cart} = props;
+const Cart = (props) => { 
+    const {cart} = props;
+    console.log(cart);
+    let total = 0 ;
+    let shipping = 0;
+    let quantity = 0; 
+    for(const product of cart){
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const taxNew = total * 0.1;
+    const taxString = taxNew.toFixed(2);
+    const tax = parseFloat(taxString);
+
+    const grandTotal = total + shipping + tax;
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order summary</h4>
-            <p>Selected Items: {cart.length}</p>
-            <h3>cart tumi aso</h3>
-            <h4>hello man</h4>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: {shipping}</p>
+            <p>Tax: {tax}</p>
+            <p>Grand Total: {grandTotal}</p>
         </div>
     );
 };
